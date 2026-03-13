dc2d75bff866adc9ee5e3c43031c8ca53a1e3e02
Zalotleh <ziadalotleh@gmail.com>
2026-03-13 13:11:57 +0100

    fix: add IF EXISTS guards to date column migration for fresh databases

--- prisma/migrations/20260116_fix_date_columns_to_timestamp/migration.sql
+++ prisma/migrations/20260116_fix_date_columns_to_timestamp/migration.sql
@@ -1,14 +1,24 @@
 -- Migration to convert DATE columns to TIMESTAMP
 -- This fixes timezone issues where dates were being stored without time information
+-- Guards added for fresh databases where these tables may not exist yet
 
--- Convert FoodConsumption.date from DATE to TIMESTAMP
-ALTER TABLE "FoodConsumption" 
-ALTER COLUMN "date" TYPE TIMESTAMP(3) USING "date"::TIMESTAMP;
+DO $$
+BEGIN
+  IF EXISTS (SELECT 1 FROM information_schema.columns WHERE table_name = 'FoodConsumption' AND column_name = 'date' AND data_type = 'date') THEN
+    ALTER TABLE "FoodConsumption" ALTER COLUMN "date" TYPE TIMESTAMP(3) USING "date"::TIMESTAMP;
+  END IF;
+END $$;
 
--- Convert DailyMenu.date from DATE to TIMESTAMP
-ALTER TABLE "DailyMenu" 
-ALTER COLUMN "date" TYPE TIMESTAMP(3) USING "date"::TIMESTAMP;
+DO $$
+BEGIN
+  IF EXISTS (SELECT 1 FROM information_schema.columns WHERE table_name = 'DailyMenu' AND column_name = 'date' AND data_type = 'date') THEN
+    ALTER TABLE "DailyMenu" ALTER COLUMN "date" TYPE TIMESTAMP(3) USING "date"::TIMESTAMP;
+  END IF;
+END $$;
 
--- Convert DailyProgressScore.date from DATE to TIMESTAMP
-ALTER TABLE "DailyProgressScore" 
-ALTER COLUMN "date" TYPE TIMESTAMP(3) USING "date"::TIMESTAMP;
+DO $$
+BEGIN
+  IF EXISTS (SELECT 1 FROM information_schema.columns WHERE table_name = 'DailyProgressScore' AND column_name = 'date' AND data_type = 'date') THEN
+    ALTER TABLE "DailyProgressScore" ALTER COLUMN "date" TYPE TIMESTAMP(3) USING "date"::TIMESTAMP;
+  END IF;
+END $$;
